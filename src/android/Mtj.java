04457151df219a54f9080a8e349cc930bfbb1516
HuangLi <huangliop@163.com>
2017-03-10 08:02:53 +0800

    添加Mjt.java

--- src/android/Mtj.java
+++ src/android/Mtj.java
@@ -1,9 +1,12 @@
 package com.baidu.mtj;
 
+import com.baidu.mobstat.StatService;
+
 import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CordovaWebView;  
+import org.json.JSONArray;
+import org.json.JSONException;
+
 
 /**
  * Created by HuangLi on 2017/3/6.
@@ -11,16 +14,20 @@ import org.apache.cordova.CordovaWebView;
 public class Mtj extends CordovaPlugin {
 
     @Override
-    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-        super.initialize(cordova, webView);
-        
-    } 
-	
-    @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 		
-        if(action.equals("startNavi")) {
-			
+        if(action.equals("onPageStart")) {
+            StatService.onPageStart(cordova.getActivity(),args.getString(0));
+        }else if (action.equals("onPageEnd")){
+            StatService.onPageEnd(cordova.getActivity(),args.getString(0));
+        }else if (action.equals("onEventStart")){
+            StatService.onEvent(cordova.getActivity(),args.getString(0),args.getString(1));
+        }else if (action.equals("onEventEnd")){
+            StatService.onEventEnd(cordova.getActivity(),args.getString(0),args.getString(1));
+        }else if (action.equals("onEvent")){
+            StatService.onEvent(cordova.getActivity(),args.getString(0),args.getString(1));
+        }else if (action.equals("onEventDuration")){
+            StatService.onEventDuration(cordova.getActivity(),args.getString(0),args.getString(1),args.getLong(2));
         }
         return super.execute(action, args, callbackContext);
     }
